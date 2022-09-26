bb989ed5a78c8de4d15a129637cb4d478a75741d
Uoiza <konkononk@gmail.com>
2022-09-26 13:31:09 +0700

    rat

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number = 100;
+
+    // console output
+    std::cout << "Hello C++"<<std::endl;
+    std::cout << number;
+    std::cout << endl;
+
+    string output = "hello," + name;
+    cout << output [1]<< endl;
+
+
+    return 0;
+
+}
